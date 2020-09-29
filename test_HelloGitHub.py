9bbc562c23790bcaabd4abf63564fae68d0fe2ac
jack-nonnie <jnonny24@gmail.com>
2020-09-29 12:41:07 -0400

    Add files via upload

--- test_HelloGitHub.py
+++ test_HelloGitHub.py
@@ -6,8 +6,20 @@ class Test(TestCase):
     def test1(self):
         self.assertEqual(helloGithub("richkempinski")[0], ['csp', 32])
     def test2(self):
-        self.assertEqual(helloGithub("richkempinski")[1], ['hellogitworld', 32])
+        temp = helloGithub("richkempinski")
+        commit = 0
+        for i in range(len(temp)):
+            if(temp[i][0] == 'hellogitworld'):
+                commit = temp[i][1]
+        self.assertEqual(commit, 32)
     def test3(self):
         self.assertEqual(helloGithub("richkempinski")[3], ['Mocks', 32])
     def test4(self):
         self.assertEqual(len(helloGithub("richkempinski")), 9)
+    def test5(self):
+        temp = helloGithub("richkempinski")
+        commitCnt = 0
+        for i in range(len(temp)):
+            if (temp[i][1] == 32):
+                commitCnt +=1
+        self.assertEqual(commitCnt, 9)
